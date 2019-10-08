4fefd9df4dbc645aec9795946c9afab96fc580af
Karl <karl.atkinson@moonpig.com>
2019-10-08 22:55:45 +0100

    add env vars config

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace eventbrite
 {
@@ -12,6 +13,10 @@ namespace eventbrite
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .ConfigureAppConfiguration((hostingContext, config) =>
+                {
+                    config.AddEnvironmentVariables();
+                })
+            .UseStartup<Startup>();
     }
 }
